f1ced243cca3df506d6ec6377fd31e837884d743
TichTa <tichta@TichTas-MacBook-Pro.local>
2022-01-25 08:53:03 +0700

    add logs request

--- app/Http/Middleware/Cafe24Auth.php
+++ app/Http/Middleware/Cafe24Auth.php
@@ -124,4 +124,25 @@ class Cafe24Auth
 
         }
     }
+    
+    /**
+     * Handle tasks after the response has been sent to the browser.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Illuminate\Http\Response  $response
+     * @return void
+     */
+    public function terminate($request, $response)
+    {
+        // log request info and runtime
+        Log::channel('request')->info('Request info: ', [
+            'mall_id' => $request->cafe_mall_id,
+            'ip' => $request->ip(),
+            'request' => [
+                'url' => $request->fullUrl(),
+                'method' => $request->method(),
+                'runtime' => microtime(true) - LARAVEL_START,
+            ]
+        ]);
+    }
 }
